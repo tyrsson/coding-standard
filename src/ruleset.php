16185d180966046a4aaad81f6798dc393ff87240
Joey Smith <jsmith@webinertia.net>
2026-01-05 22:31:14 -0600

    update ruleset file Signed-off-by: Joey Smith <jsmith@webinertia.net>
    Signed-off-by: Joey Smith <jsmith@webinertia.net>

--- src/ruleset.php
+++ src/ruleset.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 /**
  * This file is part of Webware Coding Standard.
  *
- * Copyright (c) 2021-2026 Joey Smith <jsmith@webinertia.net>
+ * Copyright (c) 2026 Joey Smith <jsmith@webinertia.net>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
